0f334d04ece651f1a3a3af2a2b1b1da256011955
Rahul0461 <rahul@qi3p.com>
2025-07-31 16:28:47 -0500

    Origin issue fixed

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,8 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://vega-digital.netlify.app" 
 ]
 
 app.add_middleware(
